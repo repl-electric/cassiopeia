112e83e2b822d36e7e811dcf70324d2f3b04299e
Joseph Wilk <joe@josephwilk.net>
2014-02-15 16:29:45 +0100

    just store base patterns

--- src/cassiopeia/data/ruchbah.clj
+++ src/cassiopeia/data/ruchbah.clj
@@ -1,12 +1,5 @@
 (ns cassiopeia.data.ruchbah)
 
-(def flow-f-buf-record (take 128 (cycle [:A3 :E3 :D3 :C4 :D3 :E3 :A3 :C3])))
-(def flow-buf-record   (take 128 (cycle [:A3 :C3 :C3 :C3 :E3 :D3 :E3 :D3 :A3 :A3 :E3 :E3 :D3 :C4 :D3 :A3])))
-(def high-pinging-record  [:E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3
-                           :E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4])
+(def flow-f-buf-record    [:A3 :E3 :D3 :C4 :D3 :E3 :A3 :C3])
+(def flow-buf-record      [:A3 :C3 :C3 :C3 :E3 :D3 :E3 :D3 :A3 :A3 :E3 :E3 :D3 :C4 :D3 :A3])
+(def high-pinging-record  [:E3 :B2 :D3 :A3 :D3 :C4 :B3 :A5 :B4 :C3 :D4 :G4 :D3 :D3 :B3 :A3 :F3])
