67c5338957b6467c6484c0ab2fc9d7661e8c2d5a
josephwilk <joe@josephwilk.net>
2014-04-01 19:39:06 +0200

    move to dropbox

--- src/cassiopeia/engine/samples.clj
+++ src/cassiopeia/engine/samples.clj
@@ -1,7 +1,7 @@
 (ns cassiopeia.engine.samples
   (:use overtone.live))
 
-(def SAMPLE-ROOT "/Users/josephwilk/Workspace/music/samples/")
+(def SAMPLE-ROOT "/Users/josephwilk/Dropbox/repl-electric/samples/")
 
 (defn load-local-sample [sample]
   (load-sample (str SAMPLE-ROOT sample)))
