aef2149f9b468d1df872a2e7bb56f5cf0defdde2
josephwilk <joe@josephwilk.net>
2014-04-09 14:04:27 +0200

    mess of experiments

--- src/cassiopeia/destination/tsih_scratch.clj
+++ src/cassiopeia/destination/tsih_scratch.clj
@@ -8,13 +8,14 @@ Experiments
           [overtone.inst.synth :as s]
           [shadertone.tone :as t])
 (:use [overtone.live]
-      [cassiopeia.scratch]
+;;      [cassiopeia.scratch]
       [cassiopeia.engine.core]
       [cassiopeia.samples]
       [cassiopeia.view-screen]
       [cassiopeia.waves.synths]))
 
 (stop)
+
 
 (do
   (ctl time/root-s :rate 4)
@@ -43,6 +44,7 @@ Experiments
   (defonce shrill-pong-buf       (buffer 128))
   (defonce shrill-pong-final-buf (buffer 128)))
 
+
 (do
   (kill high-hats)
   (kill quick-kick)
@@ -51,57 +53,167 @@ Experiments
   (reset! color-l 0.9)
   (reset! color-r 0.9)
 
-  (pattern! phase-bass-buf (repeat 3 [0 0 1 1 0 0]) [0 0 1 0 1 0])
-
-  (pattern! bass-notes-buf [:A1])
-
   (def hats
     (doall (map #(high-hats
                   [:head drums-g]
-                  :amp 0.7
-                  :mix (nth (take 32 (cycle [0.02 0.2])) %1)
-                  :room 2
-                  ;;:damp 0.6
+                  :amp 0.09
+                  :mix (nth (take 32 (cycle [1.0 1.0])) %1)
+                  :room 4
                   :note-buf bass-notes-buf
                   :seq-buf phase-bass-buf
                   :beat-bus     (:count time/beat-1th)
                   :beat-trg-bus (:beat time/beat-1th) :num-steps 32 :beat-num %1) (range 0 32))))
-  (ctl hats :damp 0.6)
+  (ctl hats :damp 1.9 :mix 0.9 :room 50)
 
-  (def qk
-    (doall (map
-            #(quick-kick
-              [:head drums-g]
-              :amp 0.5
-              :note-buf bass-notes-buf
-              :seq-buf power-kick-seq-buf
-              :beat-bus (:count time/beat-1th)
-              :beat-trg-bus (:beat time/beat-1th) :num-steps 16 :beat-num %1) (range 0 16))))
+  (kill hats)
 
-  (pattern! power-kick-seq-buf [0 0 1 0 0 0] (repeat 3 [0 0 0 0 0 0]))
+  (def kick-seq-buf          (buffer 96))
+  (def bass-notes-buf        (buffer 96))
+  (def drums-g (group "drums"))
 
-  (doseq [i (range 0 32)]
+  (doseq [i (range 0 96)]
     (kick2
      [:head drums-g]
      :note-buf bass-notes-buf
      :seq-buf  kick-seq-buf
-     :beat-bus     (:count time/beat-1th)
-     :beat-trg-bus (:beat time/beat-1th)
-     :num-steps 32
+     :beat-bus      (:count time/beat-1th)
+     :beat-trg-bus  (:beat time/beat-1th)
+     :num-steps 96
      :beat-num i))
+
+  (kill drums-g)
+
+  (stop)
+
+  (kill drums-g)
+
+  (ctl drums-g :mod-freq 1)
+  (ctl drums-g :mod-index 1.3 :noise 900)
+
+  (pattern! bass-notes-buf (repeat 5 [:A1])
+                           (repeat 2 [:A2]))
+  (pattern! phase-bass-buf (repeat 4 (repeat 4 [1 0 0 0])) [1 1 1 1])
+  (pattern! kick-seq-buf [1 0 0 0])
+  (pattern! bass-notes-buf
+            (repeat 3 (repeat 4 [:A1 :E2 :A1 :E2]))
+            (repeat 4 [:A3 :E4 :A3 :E4])
+            )
+
+  (def white (doall (map
+                     #(whitenoise-hat
+                       [:head drums-g]
+                       :amp 0.2
+                       :seq-buf  white-seq-buf
+                       :beat-bus     (:count time/beat-1th)
+                       :beat-trg-bus (:beat time/beat-1th)
+                       :num-steps 24
+                       :beat-num %1) (range 0 24))))
+
+  (kill whitenoise-hat)
+  (pattern! white-seq-buf [1])
+  (pattern! white-seq-buf (repeat 4 [1 0 0 0]) [0 0 1 1])
+
+  (pattern! kick-seq-buf [0 0 1 0 0 1 0 0 1 1])
 
   (def s (shrill-pong [:head voice-g] :amp 1.1 :note-buf shrill-pong-buf :duration-bus shrill-dur-buf :beat-bus (:count time/beat-1th) :beat-trg-bus (:beat time/beat-1th)))
 
+
+  (pattern! shrill-dur-buf
+            (repeat 4 (repeat 4 [1/12 1/12 1/2 1/2]))
+            (repeat 4 (repeat 4 [1/12 1/12 1/12 1/4]))
+            (repeat 4 [1/24 1/2 1/24 1/2]))
+
+  (pattern! shrill-dur-buf
+            (repeat 4 (repeat 4 [1/12 1/12 1/12 1/4]))
+            (repeat 4 [1/4 1/4 1/4 1/4]))
+
+  (def p (pulsar :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf pulsar-buf :amp 0.7))
+
+  (def fizzy-p (fizzy-pulsar :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf pulsar-buf :duration-bus shrill-buf))
+
+;;  (def q (shrill-pulsar :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf shrill-buf :amp 0.7))
+;;  (kill q)
+
+  (stop)
+
+  ;; (:E3 :Ab3 :B3 :D4)
+  ;; (:A3 :C#4 :E4 :Ab4)
   (let [[n1 n2 n3 n4] (chord-degree :v :A2 :major)
         [n11 n12 n13 n14] (chord-degree :i :A3 :major)]
-    (pattern! pulsar-buf      (repeat 4 [0 n2 0])          (repeat 4 [n12 n12 n12]) )
-    (pattern! shrill-buf      (repeat 2 [0 n3 0 n2 0 n4])  (repeat 2 [n13 n12 n11]))
-    (pattern! shrill-pong-buf (repeat 2 [0 n1 0 n3 0 n3])  (repeat 2 [n11 n13 n12])))
+    (pattern! pulsar-buf
+              (repeat 4 (repeat 4 [0 0 0 0]))
+              (repeat 4 [:F#4 :F#4 0 0])
+              (repeat 4 [:G#4 :G#4 0 0]))
+    (pattern! shrill-buf
+              (repeat 4 (repeat 4 [n1 n1 n3 0]))   (repeat 4 [n3 n3 n4 0]))
+    (pattern! shrill-pong-buf
+              (repeat 3 [n1 n2 n2 n3])
 
-  (pattern! growl-buf (degrees [1 1 1  1 1 1  1 1 1  1 1 1
-                                3 3 3  3 3 3  3 3 3  3 3 3
-                                5 5 5  5 5 5  5 5 5  5 5 5
-                                6 6 6  6 6 6  6 6 6  6 6 6] :major :A3))
+              (repeat 1 [n11 n13 n14 n14])
+
+              (repeat 3 [n1 n2 0 0])
+              (repeat 1 [n11 n13 n14 69])
+;;              (repeat 3 [n2 n2 n3 n3])
+;;              (repeat 1 [n1 n2 n2 n3])
+              ))
+
+
+  (let [[n1 n2 n3 n4] (chord-degree :v :A2 :harmonic-minor)
+        [n11 n12 n13 n14] (chord-degree :i :A3 :harmonic-minor)]
+    (pattern! pulsar-buf
+              (repeat 4 (repeat 4 [0 0 0 0]))
+              (repeat 4 [:F#4 :F#4 0 0])
+              (repeat 4 [:G#4 :G#4 0 0]))
+    (pattern! shrill-buf
+              (repeat 4 (repeat 4 [n1 n1 n3 0]))   (repeat 4 [n3 n3 n4 0]))
+    (pattern! shrill-pong-buf
+              (repeat 3 [n1 n2 n2 n3])
+
+              (repeat 1 [n11 n13 n14 n14])
+
+              (repeat 3 [n1 n2 0 0])
+              (repeat 1 [n11 n13 n14 69])
+              ;;              (repeat 3 [n2 n2 n3 n3])
+              ;;              (repeat 1 [n1 n2 n2 n3])
+              ))
+
+
+
+(kill p q s)
+
+(stop)
+
+  (def growl-synth (growl [:head bass-g] :amp 1 :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf growl-buf))
+
+(note :A4)
+
+  (pattern! growl-buf (degrees [1 1 1 1  1 1 1 1  1 1 1 1  1 1 1 1
+                                3 3 3 3  3 3 3 3  3 3 3 3  3 3 3 3
+                                5 5 5 5  5 5 5 5  5 5 5 5  5 5 5 5
+                                6 6 6 6  6 6 6 6  6 6 6 6  6 6 6 6
+                                7 7 7 7  7 7 7 7  7 7 7 7  7 7 7 7
+
+                                8 8 8 8  8 8 8 8  8 8 8 8  8 8 8 8
+                                ] :major :A3))
+
+  (println  (map find-note-name (chord-degree :v :A2 :major)))
+  (println (map find-note-name (chord-degree :i :A3 :major)))
+
+  ;;:E4
+  ;;F#4
+
+  (kill growl-buf)
+
+  (stop)
+
+
+
+
+
+
+
+
+  (find-note-name 66) :F#4
 
 
   (ctl time/root-s :rate 4)
@@ -109,23 +221,12 @@ Experiments
                                    [0 1 1 1 0 0])
   (pattern! white-seq-buf [0]))
 
+(stop)
+
+
 (overtime! res 0.9 0.01)
 (ctl time/root-s :rate 0)
 
-(def white (doall (map
-                   #(whitenoise-hat
-                     [:head drums-g]
-                     :amp (+ 0.09 (/  %1 10))
-                     :seq-buf  white-seq-buf
-                     :beat-bus     (:count time/beat-1th)
-                     :beat-trg-bus (:beat time/beat-1th)
-                     :num-steps 24
-                     :beat-num %1) (range 0 24))))
-
-(pattern! white-seq-buf [1 1])
-(pattern! white-seq-buf [0 0 0 1 1 0] (repeat 3 [0 0 0 0 0 0]))
-
-(pattern! kick-seq-buf [0 0 1 0 0 1 0 0 1 1])
 
 (doall (map #(glass-ping [:head glass-g] :amp 1 :note-buf bass-notes-buf :seq-buf ping-bass-seq-buf :beat-bus (:count time/beat-1th) :beat-trg-bus (:beat time/beat-1th) :num-steps 18 :beat-num %1) (range 0 18)))
 
@@ -208,10 +309,6 @@ Experiments
 
 (pattern!  kick-seq-buf [1 0 0 0])
 
-(def p (pulsar :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf pulsar-buf :amp 0.7))
-
-(def q (shrill-pulsar :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf shrill-buf :amp 0.7))
-
 (overtime! color-r 0.9)
 (def dark (dark-ambience :mul 0.2 :amp 0.4 :ring-freq (midi->hz (note :A3))))
 
@@ -245,7 +342,7 @@ Experiments
 (pattern! shrill-buf      [0 :F#3 0 :F#3 0])
 (pattern! shrill-pong-buf [0 0 :B3])
 
-(def growl-synth (growl [:head bass-g] :amp 1.8 :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-4th) :note-buf growl-buf))
+
 
 (stop)
 
@@ -365,31 +462,55 @@ Experiments
                                 "iG" (atom {:synth growl-synth :tap "g"})})
 
 
-(comment
-  (t/stop)
-  (def fx2 (fx/fx-chorus 0))
-  (def fx3 (fx/fx-echo 0))
 
-  (kill fx2)
-  (kill fx3)
 
-  (kill whitenoise-hat)
-  (kill kick2)
-  (kill high-hats)
-  (kill quick-kick)
-  (kill mid-pings)
-  (kill fizzy-pulsar)
-  (kill pulsar)
-  (kill glass-ping)
-  (kill growl)
-  (kill growler)
 
-  (kill drums-g)
-  (kill bass-g)
 
-  (kill shrill-pong)
-  (kill shrill-pulsar)
-  (kill dark-ambience)
 
-  (stop)
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(stop)
+
+
+
+
+
+
+
+
+
+(on-trigger (:trig-id time/main-beat)
+            (fn [x]
+              (when (= 0 (mod x 8))
+
+
+
+
+                )
+
+
+              )
+            ::test-trig
+
+            )
+
+(remove-event-handler ::test-trig)
--- src/cassiopeia/scratch.clj
+++ src/cassiopeia/scratch.clj
@@ -41,10 +41,17 @@
 
 (defonce silent-buffer (buffer 0))
 (def soprano-samples (doall (map (fn [idx note] [note (load-sample
-  (str (format "/Users/josephwilk/Workspace/music/samples/Soundiron Voice of Rapture - The Soprano/Samples/Sustains/Ee F/vor_sopr_sustain_ee_F_%02d" idx) ".wav"))]) (range 1 14) [60 61 62 63 64 65 66 67 68 69 70 71 72  73  74])))
+  (str (format "/Users/josephwilk/Workspace/music/samples/soprano/Samples/Sustains/Ee F/vor_sopr_sustain_ee_F_%02d" idx) ".wav"))]) (range 1 14) [60 61 62 63 64 65 66 67 68 69 70 71 72  73  74])))
 
-(defonce attack-e-soprano-samples (doall (map (fn [idx note] [note (load-sample
-                                                             (str (format "/Users/josephwilk/Workspace/music/samples/Soundiron Voice of Rapture - The Soprano/Samples/Sustains/Ee F/vor_sopr_sustain_ee_F_%02d" idx) ".wav"))]) (range 1 14) [60 61 62 63 64 65 66 67 68 69 70 71 72  73  74])))
+(def ah-strong-soprano-samples (doall (map (fn [idx note] [note (load-sample
+                                                         (str (format "/Users/josephwilk/Workspace/music/samples/soprano/Samples/Sustains/Ah F/vor_sopr_sustain_ah_F_%02d" idx) ".wav"))]) (range 1 14) [60 61 62 63 64 65 66 67 68 69 70 71 72  73  74])))
+
+(def ah-soprano-samples (doall (map (fn [idx note] [note (load-sample
+                                                         (str (format "/Users/josephwilk/Workspace/music/samples/soprano/Samples/Sustains/Ah p/vor_sopr_sustain_ah_p_%02d" idx) ".wav"))]) (range 1 14) [60 61 62 63 64 65 66 67 68 69 70 71 72  73  74])))
+
+(def yeh-soprano-samples (doall (map (fn [idx note] [note (load-sample
+                                                          (str (format "/Users/josephwilk/Workspace/music/samples/soprano/Samples/Sustains/Yeh p/vor_sopr_sustain_eh_p_%02d" idx) ".wav"))]) (range 1 14) [60 61 62 63 64 65 66 67 68 69 70 71 72  73  74])))
+
 
 (def index-buffer
   (let [b (buffer 128)]
@@ -53,17 +60,32 @@
       (buffer-set! b note (:id val)))
     b))
 
+(def ah-index-buffer
+  (let [b (buffer 128)]
+    (buffer-fill! b (:id silent-buffer))
+    (doseq [[note val] ah-soprano-samples]
+      (buffer-set! b note (:id val)))
+    b))
+
+(def yeh-index-buffer
+  (let [b (buffer 128)]
+    (buffer-fill! b (:id silent-buffer))
+    (doseq [[note val] yeh-soprano-samples]
+      (buffer-set! b note (:id val)))
+    b))
+
+
+(def ah-strong-index-buffer
+  (let [b (buffer 128)]
+    (buffer-fill! b (:id silent-buffer))
+    (doseq [[note val] ah-strong-soprano-samples]
+      (buffer-set! b note (:id val)))
+    b))
+
 (defsynth sing-f [out-bus 0 note 60 amp 0.1 pos 0]
   (let [buf (index:kr (:id index-buffer) note)
         env (env-gen (adsr :attack 2 :release 1 :sustain 1 :decay 0.2) :action FREE)]
     (out 0 (* env amp (pan2 (scaled-play-buf 1 buf) pos)))))
-
-(defonce index-buffer
-  (let [b (buffer 128)]
-    (buffer-fill! b (:id silent-buffer))
-    (doseq [[note val] soprano-samples]
-      (buffer-set! b note (:id val)))
-    b))
 
 (defsynth sing [out-bus 0 note 60 amp 0.1 pos 0]
   (let [buf (index:kr (:id index-buffer) note)
@@ -78,34 +100,136 @@
         env (env-gen (perc :release 0.4 :attack 1) :gate trg)]
     (out 0 (* env amp (pan2 (scaled-play-buf 1 buf :rate 1 :trigger trg) pos)))))
 
-(defsynth slow-singer [note-buf 0 amp 1 pos 0 release 0.2 count-b 0 beat-b 0 seq-b 0 beat-num 0 num-steps 6]
+(defsynth slow-singer [note-buf 0 amp 1 pos 0 release 0.2 count-b 0 beat-b 0 seq-b 0 beat-num 0 num-steps 6
+                       attack 0.2 release 6 decay 0.09 index-b 0]
   (let [cnt      (in:kr count-b)
         beat-trg (in:kr beat-b)
         note     (buf-rd:kr 1 note-buf cnt)
         bar-trg (and (buf-rd:kr 1 seq-b cnt)
                      (= beat-num (mod cnt num-steps))
                      beat-trg)
-
-        buf (index:kr (:id index-buffer) note)
-        env (env-gen (adsr :attack 0.2 :sustain 6 :release 6 :decay 0.09) :gate bar-trg)]
+        buf (index:kr index-b note)
+        env (env-gen (adsr :attack attack :sustain 6 :release release :decay decay) :gate bar-trg)]
     (out 0 (* amp env (pan2 (scaled-play-buf 1 buf :rate 1 :trigger bar-trg) pos)))))
 
 (defonce b (buffer 128))
 (defonce seq-b (buffer 128))
 
 (def singers-g (group "singers"))
-(def singers (doseq [i (range 0 3)]
+(def ah-singers (doseq [i (range 0 3)]
                (slow-singer
                 [:head singers-g]
                 :note-buf b :amp 2.9
-                :beat-b (:beat time/beat-12th) :count-b (:count time/beat-12th)
+                :beat-b (:beat time/beat-4th) :count-b (:count time/beat-4th)
                 :seq-b seq-b
                 :beat-num i
+                :index-b ah-index-buffer
                 :num-steps 3)))
 
+(def ahf-singers2 (doseq [i (range 0 3)]
+               (slow-singer
+                [:head singers-g]
+                :note-buf b :amp 2.9
+                :beat-b (:beat time/beat-4th) :count-b (:count time/beat-4th)
+                :seq-b b3
+                :beat-num i
+                :index-b ah-strong-index-buffer
+                :num-steps 3)))
 
-(pattern! b     [63 62 64 62 62 62 64])
-(pattern! seq-b [1 1 1])
+(def singers2 (doseq [i (range 0 3)]
+                (slow-singer
+                 [:head singers-g]
+                 :note-buf b :amp 2.9
+                 :beat-b (:beat time/beat-3th) :count-b (:count time/beat-3th)
+                 :seq-b b4
+                 :beat-num i
+                 :index-b oh-index-buffer
+                 :num-steps 3)))
+
+(kill singers-g)
+
+(def b4 (buffer 128))
+
+(pattern! b4
+          [0 0 0]
+          [0 0 0]
+          [0 0 0]
+          [0 0 0]
+          [1 0 0])
+
+(pattern! b
+          [68 69 67]
+          [69 70 68]
+          )
+
+(pattern! b  [62 63 64
+              62 63 64])
+
+
+(pattern! b4 [0 0 0 0]
+             [0 0 0 0]
+             [0 0 0 0]
+             [1 0 0 0]
+             [0 0 0 0])
+
+(def singers2 (doseq [i (range 0 3)]
+                (slow-singer
+                 [:head singers-g]
+                 :note-buf b :amp 2.9
+                 :beat-b (:beat time/beat-4th) :count-b (:count time/beat-4th)
+                 :seq-b b3
+                 :beat-num i
+                 :index-b yeah-strong-index-buffer
+                 :num-steps 3)))
+
+
+
+
+
+(def b3 (buffer 128))
+
+(pattern! b3
+          [0 0 0 0]
+          [0 0 0 0]
+          [0 0 0 0]
+          [1 0 0 0]
+          [1 0 0 0])
+
+
+(kill singers-g)
+;;(map find-note-name [63 62 64 62 62 62 64])
+(pattern! b     '(:Eb4 :D4 :E4 :D4 :D4 :D4 :E4))
+
+
+;;(pattern! b    '(68 66 65 63 64))
+(pattern! b     (shuffle [:Eb4 :D4 :E4 :D4 :D4 :D4 :E4]))
+
+(on-trigger (:trig-id time/main-beat)
+            (fn [x]
+              (when (= 0 (mod x 16)))
+              (pattern! b     (shuffle [:Eb4 :D4 :E4 :D4 :D4 :D4 :E4])))
+            ::test-trig2)
+
+(require '[overtone.inst.sampled-piano :as sp])
+
+(on-trigger (:trig-id time/main-beat)
+            (fn [x]
+              (when (= 0.0 (mod x 16))
+                (pattern! b     (shuffle [:Eb4 :D4 :E4 :D4 :D4 :D4 :E4]))))
+            ::test-trig4)
+
+
+(remove-event-handler ::test-trig4)
+
+(ctl singers-g :attack 0.2)
+(ctl singers-g :release 6.)
+
+(pattern! seq-b
+          [0 0 0 0]
+          [0 0 0 0]
+          [0 0 0 0]
+          [1 0 0 0]
+          [1 0 0 0])
 
 (defonce b2 (buffer 128))
 (defonce seq-b2 (buffer 128))
@@ -132,11 +256,7 @@
           [62 63 64]
           )
 
-
-
-(singer :note-buf b :amp 2
-        :beat-b (:beat time/beat-2th) :count-b (:count time/beat-2th) :release 10)
-
+(singer :note-buf b :amp 0.4 :beat-b (:beat time/beat-4th) :count-b (:count time/beat-4th) :release 10)
 
 (kill singer)
 (kill slow-singer)
@@ -184,3 +304,21 @@
 
   (whoosher))
 (stop)
+
+
+(defonce drums-g     (group "drums"))
+(defonce kick-seq-buf          (buffer 16))
+(defonce bass-notes-buf        (buffer 32))
+
+(doseq [i (range 0 16)]
+  (kick2
+   [:head drums-g]
+   :note-buf bass-notes-buf
+   :seq-buf  kick-seq-buf
+   :beat-bus     (:count time/beat-1th)
+   :beat-trg-bus (:beat time/beat-1th)
+   :num-steps 16
+   :beat-num i))
+
+(pattern! bass-notes-buf [:A1])
+(pattern! kick-seq-buf [1 0 0 0])
