96e8537f12d139c1521c76223f38eb988fd1a990
Joseph Wilk <joe@josephwilk.net>
2014-04-11 16:49:45 +0200

    adding some one timers

--- src/cassiopeia/destination/eta.clj
+++ src/cassiopeia/destination/eta.clj
@@ -13,7 +13,9 @@ Eta Cassiopeiae is a star system in the northern circumpolar constellation of Ca
           [shadertone.tone :as t])
 (:use [overtone.live]
       [cassiopeia.engine.core]
+      [cassiopeia.engine.scheduled-sampler]
       [cassiopeia.samples]
+      [cassiopeia.engine.samples]
       [cassiopeia.view-screen]
       [cassiopeia.waves.synths]))
 (stop)
@@ -46,7 +48,7 @@ Eta Cassiopeiae is a star system in the northern circumpolar constellation of Ca
 (def hats
     (doall (map #(high-hats
                   [:head drums-g]
-                  :amp 0.09
+                  :amp 0.18
                   :mix (nth (take 32 (cycle [1.0 1.0])) %1)
                   :room 4
                   :note-buf bass-notes-buf
@@ -75,8 +77,8 @@ Eta Cassiopeiae is a star system in the northern circumpolar constellation of Ca
           (repeat 2 [:A2]))
 (pattern! phase-bass-buf (repeat 4 (repeat 4 [1 0 0 0])) [1 1 1 1])
 (pattern! kick-seq-buf
-          (repeat 4 (repeat 4 [1 0 0 0]))
-          (repeat 2 (repeat 4 [1 1 0 0])))
+          (repeat 2 (repeat 4 [1 0 0 0]))
+          (repeat 4 (repeat 4 [1 0 0 0])))
 (pattern! bass-notes-buf
           (repeat 4 (repeat 4 [:A1 :A1 :A1 :A1]))
           (repeat 2 (repeat 4 [:E#1 :E#1 :E#1 :E#1])))
@@ -96,11 +98,11 @@ Eta Cassiopeiae is a star system in the northern circumpolar constellation of Ca
 
 (def s (shrill-pong [:head voice-g] :amp 0.2 :note-buf shrill-pong-buf :duration-bus shrill-dur-buf :beat-bus (:count time/beat-1th) :beat-trg-bus (:beat time/beat-1th)))
 
-(ctl s :amp 1.1)
+(node-over-time s :amp 0.2 1.1 0.01)
 
 (pattern! shrill-dur-buf
-          (repeat 4 (repeat 4 [1/64 1/64 1/64 1/64]))
-          (repeat 4 [1/128 1/128 1/64 1/64])
+          (repeat 4 (repeat 4 [1/32 1/32 1/32 1/32]))
+;;          (repeat 4 [1/128 1/128 1/64 1/64])
           )
 
 (pattern! shrill-dur-buf
@@ -143,14 +145,17 @@ Eta Cassiopeiae is a star system in the northern circumpolar constellation of Ca
             (repeat 4 (repeat 4 [0 0 0 0])))
   )
 
-(def growl-synth (growl [:head bass-g] :amp 1 :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf growl-buf))
+(def growl-synth (growl [:head bass-g] :amp 0 :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf growl-buf))
 
 (node-over-time growl-synth :amp  1 0.0 0.01)
+(node-over-time growl-synth :amp  0 0.9 0.01)
 
 (pattern! growl-buf (degrees [1 1 1 1  1 1 1 1  1 1 1 1  1 1 1 1
                               3 3 3 3  3 3 3 3  3 3 3 3  3 3 3 3] :major :A2))
 
-(def growl-synth (growl [:head bass-g] :amp 1 :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf growl2-buf))
+(def growl-synth (growl [:head bass-g] :amp 0 :beat-trg-bus (:beat time/beat-1th) :beat-bus (:count time/beat-1th) :note-buf growl2-buf))
+
+(node-over-time growl-synth :amp 0 1 0.01)
 
 (pattern! growl2-buf (degrees [1 1 1 1  1 1 1 1  1 1 1 1  1 1 1 1
                               3 3 3 3  3 3 3 3  3 3 3 3  3 3 3 3
@@ -160,3 +165,6 @@ Eta Cassiopeiae is a star system in the northern circumpolar constellation of Ca
                               8 8 8 8  8 8 8 8  8 8 8 8  8 8 8 8] :major :A3))
 
 (stop)
+(def mm-s (load-sample "/Users/josephwilk/Workspace/music/samples/soprano/Samples/Sustains/Mm p/vor_sopr_sustain_mm_p_03.wav"))
+(schedule-sample mm-s time/main-beat :mod-size 64 :amp 0.2)
+(schedule-sample whisper-s time/main-beat :mod-size 64 :amp 0.09)
