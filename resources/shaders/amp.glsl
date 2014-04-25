7eba0c3f2b1f0fa0ad781a5368a45b6d0dded916
Joseph Wilk <joe@josephwilk.net>
2014-04-25 13:26:05 +0200

    cleaning up old shader this was born from.

--- resources/shaders/amp.glsl
+++ resources/shaders/amp.glsl
@@ -9,11 +9,13 @@ uniform float iExpand;
 uniform float iYinYan;
 uniform float iOvertoneVolume;
 uniform float iBeat;
+uniform float iBeatCounter;
 
 const float scale=50.5;
 const float detail=50.5;
 const float width=0.001;
 
+const int ampMode=1;
 const int smoothWave=0;
 const float waveReducer=.03;
 const int lightOn=0;
@@ -90,7 +92,6 @@ vec4 generateWave(vec2 uv, float yOffset, float orien){
     //wave = (-1.0 * wave)+0.5;
   }
   vec3  wc     = wave * hsv2rgb(fract(iGlobalTime/colorChangeRate),iLColor,iRColor);
-//0.1 0.1 0.9 0.9
 
   float zf     = -0.05;
   vec2  uv2    = (1.0+zf)*uv-(zf/2.0,zf/2.0);
@@ -104,66 +105,49 @@ void main(void)
   float space = 0.1;
   float res = 0.75;
 
-  vec2 uv;
-  vec2 uv2;
-  vec2 uv3;
-
-  uv     = (gl_FragCoord.xy / iResolution.xy);
-  uv2    = (gl_FragCoord.xy / iResolution.xy);
-  uv3    = (gl_FragCoord.xy / iResolution.xy);
-
-  //uv.x = uv.x * iBeat;
-  //uv3.x = uv3.x / iBeat;
-  // uv.x = uv.x * 3.14159265359;
+  vec2 uv = (gl_FragCoord.xy / iResolution.xy);
+  vec2 uv1 = (gl_FragCoord.xy / iResolution.xy);;
 
   //Circle background
   vec2 pos = mod(gl_FragCoord.xy, vec2(5.0)) - vec2(0.0);
   float dist_squared = dot(pos, pos);
   vec4 c = (dist_squared < 0.6) ? vec4(.0, .0, .0, 0.0): vec4(1.0, 1.0, 1.0, 1.0);
 
-  //vec2 pos = mod(gl_FragCoord.xy, vec2(50.0)) - vec2(25.0);
-  //float dist_squared = dot(pos, pos);
-  //vec4 c = mix(vec4(.90, .90, .90, 1.0), vec4(.20, .20, .40, 1.0),
-  //             smoothstep(380.25, 420.25, dist_squared));
-  //  float radius = 10.0;
-  //float dist = 10;
-  //vec2 circle = smoothstep(radius-1.0, radius+1.0, dist);
-
-
-  //  float dist_sq = dot(uv.x, uv.x);
-  //uv.x = dist_sq;
-  //uv2.x = dist_sq;
-  //uv3.x = dist_sq;
   float radius = 0.29;
   float scale = 32.1;
-  uv = gl_FragCoord.xy/iResolution.xy;
-
   float noCircles = 1.0;
 
-  uv.x = (radius * cos(uv.x*(6.2*noCircles)));
-  uv.y = (radius * sin(uv.y*(3.6*noCircles)));
-  uv.x = uv.x+0.90+(clamp(0.01,0.02,iBeat));
-  uv.y = uv.y*0.9;
+  uv.x = (radius * cos(uv.x * (6.2 * noCircles)));
+  uv.y = (radius * sin(uv.y * (3.6 * noCircles)));
+  uv.x = uv.x + 0.90 + (clamp(0.01,0.02,iBeat));
+  uv.y = uv.y * 0.9;
 
-  vec4  wave1  = generateWave(uv,  0.0, uv.x);
-  vec4  wave2  = generateWave(uv,  0.1, uv.x);
-  vec4  wave3  = generateWave(uv,  0.05, uv.x);
+  vec4 w;
+  if(ampMode==1){
+    vec4  wave1  = generateWave(uv,  0.0, uv.x);
+    vec4  wave2  = generateWave(uv,  0.1, uv.x);
+    vec4  wave3  = generateWave(uv,  0.05, uv.x);
 
-  //  vec4  wave2  = generateWave(uv2,  0.3, -uv2.x * iExpand);
-  //  vec4  wave3  = generateWave(uv3, 0.2,  1-uv3.x * iExpand);
-  vec4  wave4  = generateWave(uv3, 0.25, -uv3.x * iExpand);
+    w = c * mix(wave3,mix(wave1, wave2,0.5),0.5);
+  }
+  else{
+    vec4  wave1  = generateWave(uv1, 0.1, -uv1.x   * iExpand);
+    vec4  wave2  = generateWave(uv1, 0.3, -uv1.x     * iExpand);
+    vec4  wave3  = generateWave(uv1, 0.2, 1-uv1.x * iExpand);
+    vec4  wave4  = generateWave(uv1, 0.25,uv1.x  * iExpand);
 
-  vec4 w = mix(mix(mix(wave1,wave2,0.5), wave3, 0.5), wave4,0.5);
+    w = mix(mix(mix(wave1,wave2,0.5), wave3, 0.5), wave4,0.5);
+  }
 
   if(lightOn==1){
     vec3 from=vec3(0.,0.1,-1.2);
     vec3 dir=normalize(vec3(uv,1.));
     float col = light(from, dir);
     col = col;
-    gl_FragColor = w*vec4(col);
+    gl_FragColor = w * vec4(col);
   }
   else{
-    gl_FragColor = c*mix(wave3,mix(wave1, wave2,0.5),0.5);
+    gl_FragColor = w;
   }
 
 }
