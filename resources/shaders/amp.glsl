d29d49136d39c89a59ca71e3e582003cc9249ed2
Joseph Wilk <joe@josephwilk.net>
2014-04-26 23:55:27 +0200

    Experiment with distortions

--- resources/shaders/amp.glsl
+++ resources/shaders/amp.glsl
@@ -20,8 +20,50 @@ const int smoothWave=0;
 const float waveReducer=.03;
 const int lightOn=0;
 
-vec3 lightdir=-vec3(.1,.0,0.);
-  float motion(vec3 p) {
+vec3 lightdir = vec3(.1,.0,0.);
+
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+float snoise(vec2 v){
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m;
+  m = m*m;
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  m *= 1.79284291400159 - 0.85373472095314 * (a0*a0 + h*h);
+
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+float motion(vec3 p) {
   float acceleration = 10.0;
   float movementReducer = 10.0;
 
@@ -35,7 +77,7 @@ vec3 lightdir=-vec3(.1,.0,0.);
    d = min(d,length(p.xy) - width);
    d = min(d,length(p*p*p)-width*.3);
    return d*dotp/scale;
- }
+}
 
 vec3 normal(vec3 p) {
   vec3 e = vec3(0.0, detail, 0.0);
@@ -95,6 +137,7 @@ vec3 hsv2rgb(float mixRate,float v) {
 
 vec4 generateWave(vec2 uv, float yOffset, float orien){
   float centerOffset=1.4;
+
   vec4 wa = texture2D(iChannel0, vec2(uv.x, iRes*2.6));
   float wave = waveReducer*wa.x;
 
@@ -113,6 +156,10 @@ vec4 generateWave(vec2 uv, float yOffset, float orien){
 
 void main(void)
 {
+
+  float smoothedVolume;
+  smoothedVolume += (iOvertoneVolume  - smoothedVolume) * 0.1;
+
   float space = 0.1;
   float res = 0.75;
 
@@ -127,6 +174,7 @@ void main(void)
   float radius = 0.29;
   float scale = 32.1;
   float noCircles = 1.0;
+
 
   uv.x = (radius * cos(uv.x * (6.2 * noCircles)));
   uv.y = (radius * sin(uv.y * (3.6 * noCircles)));
@@ -150,7 +198,40 @@ void main(void)
     w = mix(mix(mix(wave1,wave2,0.5), wave3, 0.5), wave4,0.5);
   }
 
-  w = textureCutout(w, texture2D(iChannel2, vec2(uv1.x, -uv1.y)));
+  // make some noise
+
+  vec2 p = uv1;
+  float distortion=0.0001;
+  float distortion2=0.8;
+  float speed=0.05;
+  float rollSpeed=0.0;
+  float ty = iGlobalTime* speed;
+  float yt = p.y - ty;
+  float offset = snoise(vec2(yt*3.0,0.0))*0.2;
+  offset = pow(offset*distortion, 3.0)/distortion ;
+  offset += snoise(vec2(yt*50.0,0.0))*distortion2*0.001;
+
+  vec2 o = vec2(fract(p.x + offset),-fract(p.y-iGlobalTime*rollSpeed));
+
+  vec4 cTextureScreen = w;
+  float sCount = 900.;
+  float nIntensity=0.9;
+  float sIntensity=0.8;
+  // sample the source
+  float x = uv1.x * uv1.y * iGlobalTime *  1000.0;
+  x = mod( x, 13.0 ) * mod( x, 123.0 );
+  float dx = mod( x, 0.01 );
+  vec3 cResult = cTextureScreen.rgb + cTextureScreen.rgb * clamp( 0.1 + dx * 100.0, 0.0, 1.0 );
+  // get us a sine and cosine
+  vec2 sc = vec2( sin( uv1.y * sCount ), cos( uv1.y * sCount ) );
+  // add scanlines
+  cResult += cTextureScreen.rgb * vec3( sc.x, sc.y, sc.x ) * sIntensity;
+  // interpolate between source and result by intensity
+  cResult = cTextureScreen.rgb + clamp( nIntensity, 0.0,1.0 ) * ( cResult - cTextureScreen.rgb );
+
+  vec4 f = vec4(cResult, cTextureScreen.a);
+
+  vec4 cutout = textureCutout(f, texture2D(iChannel2, o));
 
   if(lightOn==1){
     vec3 from=vec3(0.,0.1,-1.2);
@@ -160,7 +241,7 @@ void main(void)
     gl_FragColor = w * vec4(col);
   }
   else{
-    gl_FragColor = w;
+    gl_FragColor = cutout;
   }
 
 }
