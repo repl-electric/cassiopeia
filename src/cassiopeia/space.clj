31e8915b1a654b93b263a62409bd25166bbf6f7a
Joseph Wilk <joe@josephwilk.net>
2014-04-30 17:34:18 +0200

    needed for sample play back

--- src/cassiopeia/space.clj
+++ src/cassiopeia/space.clj
@@ -2,7 +2,7 @@
 "
 Repl Electric
 "
-(:use [overtone.live] [cassiopeia.waves.synths] [cassiopeia.engine.core] [cassiopeia.waves.soprano]       [cassiopeia.view-screen])
+(:use [overtone.live] [cassiopeia.waves.synths] [cassiopeia.engine.core] [cassiopeia.waves.soprano] [cassiopeia.view-screen] [cassiopeia.engine.core] [cassiopeia.engine.scheduled-sampler])
 (:require [cassiopeia.engine.timing :as time] [overtone.studio.fx :as fx]  [shadertone.tone :as t]))
 
 (ctl time/root-s :rate 4)
