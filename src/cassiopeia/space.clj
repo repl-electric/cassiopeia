530df30705571c8bc2d1135acbcf3b017dc44407
Joseph Wilk <joe@josephwilk.net>
2014-05-06 13:23:44 +0200

    access samples

--- src/cassiopeia/space.clj
+++ src/cassiopeia/space.clj
@@ -2,7 +2,7 @@
 "
 Repl Electric
 "
-(:use [overtone.live] [cassiopeia.waves.synths] [cassiopeia.engine.core] [cassiopeia.waves.soprano] [cassiopeia.view-screen] [cassiopeia.engine.core] [cassiopeia.engine.scheduled-sampler])
+(:use [overtone.live] [cassiopeia.waves.synths] [cassiopeia.engine.core] [cassiopeia.waves.soprano] [cassiopeia.view-screen] [cassiopeia.engine.core] [cassiopeia.engine.scheduled-sampler] [cassiopeia.samples])
 (:require [cassiopeia.engine.timing :as time] [overtone.studio.fx :as fx]  [shadertone.tone :as t]))
 
 (ctl time/root-s :rate 4)
