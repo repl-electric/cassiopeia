7c84636f9997ff2c017233dddc462ca3e3dc2939
Joseph Wilk <joe@josephwilk.net>
2014-05-09 13:44:35 +0200

    formatting

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
                [launchpad "0.1.0-SNAPSHOT"]
                [monome "0.1.0-SNAPSHOT"]
 
-;;               [shadertone "0.2.3"]
-	[shadertone "0.2.4-SNAPSHOT"]
+               ;;[shadertone "0.2.3"]
+               [shadertone "0.2.4-SNAPSHOT"]
 
                [rogerallen/leaplib "0.8.1"]
                [rogerallen/leaplib-natives "0.8.1"]
