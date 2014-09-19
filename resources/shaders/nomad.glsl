abe35bb970b3433349999e027b8f4afebaed1acb
Joseph Wilk <joe@josephwilk.net>
2014-09-19 18:18:44 -0500

    random stuff

--- resources/shaders/nomad.glsl
+++ resources/shaders/nomad.glsl
@@ -15,6 +15,7 @@ uniform float iMeasureCount;
 #define TOTAL_BEATS 128.0
 #define STATIC_LETTERS 0
 #define SHOW_GLOW 1
+#define PANIC 0
 
 float rand(vec2 co){
   return fract(sin(dot(co.xy ,vec2(2.9898,78.233))) * 58.5453);
@@ -202,6 +203,25 @@ vec4 addGlow(vec2 uv, vec2 v, float glow)
   return glowing;
 }
 
+vec4 lineDistort(vec4 cTextureScreen, vec2 uv1){
+  float sCount = 900.;
+  float nIntensity=0.1;
+  float sIntensity=0.2;
+  // sample the source
+  float x = uv1.x * uv1.y * iGlobalTime *  1000.0;
+  x = mod( x, 13.0 ) * mod( x, 123.0 );
+  float dx = mod( x, 0.05 );
+  vec3 cResult = cTextureScreen.rgb + cTextureScreen.rgb * clamp( 0.1 + dx * 100.0, 0.0, 1.0 );
+  // get us a sine and cosine
+  vec2 sc = vec2( sin( uv1.y * sCount ), cos( uv1.y * sCount ) );
+  // add scanlines
+  cResult += cTextureScreen.rgb * vec3( sc.x, sc.y, sc.x ) * sIntensity;
+  // interpolate between source and result by intensity
+  cResult = cTextureScreen.rgb + clamp(nIntensity, 0.0,1.0 ) * (cResult - cTextureScreen.rgb);
+
+  return vec4(cResult, cTextureScreen.a);
+}
+
 vec4 buildCell(vec2 uv, vec2 point, int still){
   float person = 1.0;
   float inc;
@@ -246,7 +266,7 @@ vec4 buildCell(vec2 uv, vec2 point, int still){
 
   if (cell > 0.0001){
     person -= 1.0;
-  }else if (cell < 0.001){
+  }else if (cell < 0.0001){
     person -= 0.9;
   }
   vec4 helloPoint = vec4(vec3(person),1.0);
@@ -295,8 +315,11 @@ vec4 bouncingPerson(vec2 uv){
 
   if(iOvertoneVolume > 0.1){
     helloPoint += letter(letterE, vec2(0.3+letterSpace*2, 0.45), uv);
-   helloPoint += letter(letterP, vec2(0.3+letterSpace*4, 0.45), uv);
-    helloPoint += letter(letterL, vec2(0.3+letterSpace*6, 0.45), uv);
+
+    if(PANIC == 0){
+    helloPoint += letter(letterP, vec2(0.3+letterSpace*4, 0.45), uv);
+     helloPoint += letter(letterL, vec2(0.3+letterSpace*6, 0.45), uv);
+    }
   }
 
   if(RANDOM_LETTERS == 0){
@@ -342,13 +365,86 @@ vec4 theCell(vec2 uv){
 
   return x;
 }
+
+
+vec3 mod289(vec3 x){
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x){
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+float snoise(vec2 v){
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m;
+  m = m*m;
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  m *= 1.79284291400159 - 0.85373472095314 * (a0*a0 + h*h);
+
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+vec2 warpedCords(vec2 p){
+  float distortion=0.0001;
+  float distortion2=0.5;
+  float speed=0.05;
+  float rollSpeed=0.0;
+  float ty = iGlobalTime* speed;
+  float yt = p.y - ty;
+  float offset = snoise(vec2(yt*3.0,0.0))*0.2;
+  offset = pow(offset*distortion, 3.0)/distortion ;
+  offset += snoise(vec2(yt*50.0,0.0))*distortion2*0.001;
+
+  vec2 o = vec2(fract(p.x + offset),-fract(p.y-iGlobalTime*rollSpeed));
+  return o;
+}
+
+
+vec4 generateSnow(vec2 p, vec4 tex){
+  float size = 2.;
+  vec4 color = tex;
+  float amount=0.3;
+  float xs = floor(gl_FragCoord.x / size);
+  float ys = floor(gl_FragCoord.y / size);
+  vec4 snow = vec4(rand(vec2(xs,ys*iGlobalTime/1800))*amount);
+  return snow;
+}
+
+
+
 void main(void){
   vec2 uv = gl_FragCoord.xy / iResolution.x;
+
+
 
   float noiseWeight = 0.0;
   float hexWeight   = 0.0;
   float populationWeight = 0.0;
-  float spellWeight = 1.0;
+  float spellWeight = 0.0;
 
   vec4 leftNoise  = vec4(0.);
   vec4 rightNoise = vec4(0.);
