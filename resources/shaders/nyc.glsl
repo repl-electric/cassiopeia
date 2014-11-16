9f0045956458f74547b35fab170377e3d5c4ebda
Joseph Wilk <joe@josephwilk.net>
2014-11-16 19:12:20 +0100

    Look to phoenix example for inspiration.

--- resources/shaders/nyc.glsl
+++ resources/shaders/nyc.glsl
@@ -199,9 +199,86 @@ vec4 generateSnow(vec2 p, float speed){
   return snow;
 }
 
+
+// original by nimitz https://www.shadertoy.com/view/lsSGzy#, slightly modified
+
+#define ray_brightness 0.3
+#define gamma 1.0
+#define ray_density 4.5
+#define curvature 15.
+#define red   4.
+#define green 1.0
+#define blue  .1
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!! UNCOMMENT ONE OF THESE TO CHANGE EFFECTS !!!!!!!!!!!
+// MODE IS THE PRIMARY MODE
+#define MODE normalize
+// #define MODE
+
+#define MODE3 *
+//#define MODE3 +
+
+#define MODE2 r +
+//#define MODE2
+
+#define DIRECTION +
+//#define DIRECTION -
+
+#define SIZE 0.1
+
+#define INVERT /
+// #define INVERT *
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+float noise( in vec2 x )
+{
+  return texture2D(iChannel2, x*.01).x; // INCREASE MULTIPLIER TO INCREASE NOISE
+}
+
+// FLARING GENERATOR, A.K.A PURE AWESOME
+mat2 m2 = mat2( 0.80,  0.60, -0.60,  0.80 );
+float fbm( in vec2 p )
+{
+  float z=5.;       // EDIT THIS TO MODIFY THE INTENSITY OF RAYS
+  float rz = 0.09; // EDIT THIS TO MODIFY THE LENGTH OF RAYS
+  p *= 0.25;        // EDIT THIS TO MODIFY THE FREQUENCY OF RAYS
+  for (int i= 1; i < 6; i++)
+    {
+      rz+= abs((noise(p)-0.5)*2.)/z;
+      z = z*2.;
+      p = p*2.*m2;
+    }
+  return rz;
+}
+
+vec4 flare(void)
+{
+  float t = DIRECTION iGlobalTime*.3;
+  vec2 uv = gl_FragCoord.xy / iResolution.xy-0.5;
+  uv.x *= iResolution.x/iResolution.y;
+  uv*= curvature* SIZE;
+
+  float r = sqrt(dot(uv,uv)); // DISTANCE FROM CENTER, A.K.A CIRCLE
+  float x = dot(MODE(uv), vec2(.5,0.))+t;
+  float y = dot(MODE(uv), vec2(.0,.5))+t;
+
+  float val;
+  val = fbm(vec2(MODE2 y * ray_density, MODE2 x MODE3 ray_density)); // GENERATES THE FLARING
+  val = smoothstep(gamma*.02-.1,ray_brightness+(gamma*0.02-.1)+.001,val);
+  val = sqrt(val); // WE DON'T REALLY NEED SQRT HERE, CHANGE TO 15. * val FOR PERFORMANCE
+
+  vec3 col = val INVERT vec3(red,green,blue);
+  col = 1.-col; // WE DO NOT NEED TO CLAMP THIS LIKE THE NIMITZ SHADER DOES!
+  float rad= 0.2 * texture2D(iChannel0, vec2(0,0.25)).x; // MODIFY THIS TO CHANGE THE RADIUS OF THE SUNS CENTER
+  col = mix(col,vec3(1.), rad - 266.667 * r); // REMOVE THIS TO SEE THE FLARING
+
+  return vec4(col,1.0);
+}
+
 void main(void){
   vec2 uv = gl_FragCoord.xy / iResolution.x;
-  float snowWeight = 0.0;
+  float snowWeight = 0.3;
   float snowSpeed = 0.000000000001; //0.00000001; //0.0000000001;
   vec4 populationResult = vec4(0., 0., 0., 0.);
 
@@ -234,7 +311,7 @@ void main(void){
     c = 1.0-(circularWeight*circular()) -  (1.0-(snowWeight * generateSnow(uv, snowSpeed))) - populationResult;
   }
   else{
-    c = (circularWeight*circular()) - ((snowWeight * generateSnow(uv, snowSpeed))) + populationResult;
+    c = (circularWeight*circular()) - ((snowWeight * generateSnow(uv, snowSpeed))) + populationResult + flare() * 0.01;
   }
   gl_FragColor = c;
 }
