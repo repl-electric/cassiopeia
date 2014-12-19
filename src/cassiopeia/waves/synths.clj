1e24125b04c0ae7ae80fa4b0feea00735599fdc3
Joseph Wilk <joe@josephwilk.net>
2014-12-19 13:12:38 +0100

    Version of seqer with only one instance

--- src/cassiopeia/waves/synths.clj
+++ src/cassiopeia/waves/synths.clj
@@ -39,6 +39,22 @@
                          :amp amp)]
     (out 0 (* amp src))))
 
+(defsynth efficient-seqer
+  "Plays a single channel audio buffer. Only uses a single synth"
+  [buf 0 rate 1 out-bus 0 beat-num 0 pattern 0 beat-bus (:count time/main-beat) beat-trg-bus (:beat time/main-beat) amp 0.7
+   rate-start 0.1
+   rate-limit 0.9]
+  (let [cnt      (in:kr beat-bus)
+        rander (mod cnt 1)
+        beat-trg (in:kr beat-trg-bus)
+        bar-trg  (and (buf-rd:kr 1 pattern cnt)
+                      beat-trg)
+        vol      (set-reset-ff bar-trg)]
+    (out out-bus (* vol amp (scaled-play-buf :num-channels 1
+                                             :buf-num buf
+                                             :rate (t-rand:kr rate-start rate-limit rander)
+                                             :trigger bar-trg)))))
+
 (defsynth seqer
   "Plays a single channel audio buffer."
   [buf 0 rate 1 out-bus 0 beat-num 0 pattern 0  num-steps 8 beat-bus (:count time/main-beat) beat-trg-bus (:beat time/main-beat) amp 0.7
