621c45e2a7aa821f962d8200e4e589becb2e51be
Joseph Wilk <joe@josephwilk.net>
2015-01-05 20:58:59 +0100

    playing with expression

--- src/cassiopeia/destination/flatiron/scores.clj
+++ src/cassiopeia/destination/flatiron/scores.clj
@@ -7,8 +7,8 @@
 (def dark-chords-score
   (let [_ [0 0 0 0]
         [f31 f32 f33 f34 f35 f36 f37] (chords-for :F3 :minor 3)]
-    (chords-seq :minor [:F3 :1*16 :3*16 :4*16 :6*8 :m+5*8
-                        :F3 :1*16 :3*16 :4*16 :6*8 :m7+5*8])
+    (chords-seq [:F3 :1*16 :3*16 :4*16 :6*8 :m+5*8
+                 :F3 :1*16 :3*16 :4*16 :6*8 :m7+5*8])
     (concat
      (repeat 16 f31)
      (repeat 16 f33)
@@ -51,8 +51,9 @@
         [f21c f22c f23c f24c f25c f26c f27c] (chords-with-inversion [1 2] :F2 :minor :up 3)
         [f21 f22 f23 f24 f25 f26 f27]        (chords-for :F2 :minor 3)]
 
-    (chords-seq :minor [:F2 :1c*8 :6*8  :3b*8 :4b*6 :sus4c*2
-                        :F2 :1c*8 :1c*8 :3b*8 :5b*6 :7sus4c*2])
+    ;;  (chords-seq [:F2 :1c*8 :6*8  :3b*8 :4b*6 :sus4c*2  :F2 :1c*8 :1c*8 :3b*8 :5b*6 :7sus4c*2] :minor)
+    ;;  (chords-seq "F2 1c*8 6*8  3b*8 4b*6 sus4c*2 F2 1c*8 1c*8 3b*8 5b*6 7sus4c*2" :minor)
+
     (concat
      (repeat 8 f21c)
      (repeat 8 f26)
@@ -102,6 +103,17 @@
         [f31 f32 f33 f34 f35 f36 f37]        (chords-for :F3 :minor 1)
         [f41 f42 f43 f44 f45 f46 f47]        (chords-for :F4 :minor 1)
         [f312 f322 f332 f342 f352 f362 f372] (chords-for :F3 :minor 2)]
+
+    (comment
+      (degrees-seq :minor
+                   (repeat 2 [:F4 1 3 1     4 :F3 7 :C3 6 :F3 7     7])
+                   (repeat 2 [:F4 1 3 1     4 :C3 7 :C3 6 :F3 7     7])
+
+                   (repeat 2 [:F4 1 3 1     4 :F3 7 :C3 6 :F3 7     7])
+                   [:F4 1 3 1     4 :C4 3 :C3 6 :F3 7     7]
+                   [:F4 1 3 1 :C4 4     7     6 :C4 3 :F3 5]
+                   ))
+
 
     [f41 f43 f41 f44 f37 c36 (as-chord (degrees [7] :minor :F3)) (as-chord (degrees [7] :minor :F3))
      f41 f43 f41 f44 f37 c36 (as-chord (degrees [7] :minor :F3)) (as-chord (degrees [7] :minor :F3))
