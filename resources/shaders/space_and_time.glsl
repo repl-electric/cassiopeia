159e16e1d434aea3bbe981e56b39653be14d72ed
Joseph Wilk <joe@josephwilk.net>
2015-03-15 14:33:48 +0100

    play

--- resources/shaders/space_and_time.glsl
+++ resources/shaders/space_and_time.glsl
@@ -95,16 +95,75 @@ vec4 textureCutout(vec4 w, vec4 tex){
   return w;
 }
 
+vec3 mod289(vec3 x){
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x){
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+float snoise(vec2 v){
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m;
+  m = m*m;
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  m *= 1.79284291400159 - 0.85373472095314 * (a0*a0 + h*h);
+
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+vec2 warpedCords(vec2 p){
+  float distortion=0.0001;
+  float distortion2=0.5;
+  float speed=0.02;
+  float rollSpeed=0.0;
+  float ty = iGlobalTime* speed;
+  float yt = p.y - ty;
+  float offset = snoise(vec2(yt*3.0,0.0))*0.2;
+  offset = pow(offset*distortion, 3.0)/distortion ;
+  offset += snoise(vec2(yt*50.0,0.0))*distortion2*0.001;
+  vec2 o = vec2(fract(p.x + offset), fract(p.y-iGlobalTime*rollSpeed));
+  return o;
+}
+
 void main(void){
   vec2 uv = gl_FragCoord.xy / iResolution.x;
   vec4 r = generateSpaceLights(uv) + vec4(hsvToRgb(0.0,0.0),1.0);
 
   float time = 1.0;
   float space = 0.0;
+  float blurWeight = 1.0;
 
-  uv.y+= 0.15;
-  r = r- time*(textureCutout(vec4(0.0,0.0,0.0,1.0), texture2D(iChannel2, uv)));
-  r = r- space*(textureCutout(vec4(0.0,0.0,0.0,1.0),texture2D(iChannel3, uv)));
+  uv.y+= 0.0;
+  //  uv.x= uv.x*1.0;
+  //  uv.y= uv.y*1;
+
+  r = r- time*(textureCutout(vec4(0.0,0.0,0.0,1.0), texture2D(iChannel2, warpedCords(uv))));
+  r = r- space*(textureCutout(vec4(0.0,0.0,0.0,1.0),texture2D(iChannel3, warpedCords(uv))));
 
 
   gl_FragColor = r; //lineDistort(r, uv);
