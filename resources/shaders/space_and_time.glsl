8657b3cbec632bd2ed05495002345836d62631a8
Joseph Wilk <joe@josephwilk.net>
2015-03-15 15:10:01 +0100

    playing with bluring FFT

--- resources/shaders/space_and_time.glsl
+++ resources/shaders/space_and_time.glsl
@@ -88,6 +88,60 @@ vec4 generateSpaceLights(vec2 uv1){
   return vec4(col, 1.0);
 }
 
+// Bluring fn froms:
+// Bokeh disc.
+// by David Hoskins.
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#define PI 3.141596
+
+// This is (3.-sqrt(5.0))*PI radians, which doesn't precompiled for some reason.
+// The compiler is a dunce I tells-ya!!
+#define GOLDEN_ANGLE 2.39996323
+
+#define NUMBER 150.0
+
+#define ITERATIONS (GOLDEN_ANGLE * NUMBER)
+
+//-------------------------------------------------------------------------------------------
+// This creates the 2D offset for the next point.
+// (r-1.0) is the equivalent to sqrt(0, 1, 2, 3...)
+vec2 Sample(in float theta, inout float r){
+  r += 1.0 / r;
+  return (r-1.0) * vec2(cos(theta), sin(theta));
+}
+
+vec3 Bokeh(sampler2D tex, vec2 uv, float radius, float amount){
+  vec3 acc = vec3(0.0);
+  vec3 div = vec3(0.0);
+  vec2 pixel = vec2(iResolution.y/iResolution.x, 1.0) * radius * .002;
+  float r = 1.0;
+  for (float j = 0.0; j < ITERATIONS; j += GOLDEN_ANGLE){
+      vec3 col = texture2D(tex, uv + pixel * Sample(j, r)).xyz;
+      col = col * col * 1.4; // ...contrast it for better highlights
+      vec3 bokeh = vec3(5.0) + pow(col, vec3(9.0)) * amount;
+      acc += col * bokeh;
+      div += bokeh;
+  }
+  return acc / div;
+}
+
+vec4 blur(vec2 uv){
+  //vec2 uv = fragCoord.xy / iResolution.xy;
+  float time = iGlobalTime*.2 + .5;
+  float r = .7 - .7*cos(time * PI * 2.0);
+
+  float a = 150.0;
+  if (iMouse.w >= 1.0)
+    {
+      r = (iMouse.x/iResolution.x)*3.0;
+      a = iMouse.y/iResolution.y * 80.0;
+    }
+
+  uv *= vec2(1.0, -1.0);
+
+  return vec4(Bokeh(iChannel0, uv, r, a), 1.0);
+}
+
 vec4 textureCutout(vec4 w, vec4 tex){
   vec4 logoAlpha = tex.aaaa;
   vec4 negAlpha = logoAlpha * vec4(-1.,-1.,-1.,0.) + vec4(1.,1.,1.,0.);
