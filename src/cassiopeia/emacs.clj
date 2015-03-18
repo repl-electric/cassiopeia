de0680695f63d7e7aec997265dfe792953eed01d
Joseph Wilk <joe@josephwilk.net>
2015-03-18 22:13:46 +0100

    silly

--- src/cassiopeia/emacs.clj
+++ src/cassiopeia/emacs.clj
@@ -1,8 +1,9 @@
 (ns cassiopeia.emacs
-  (:use [overtone.core]))
+  (:use [overtone.core] [mud.core] [mud.timing] [cassiopeia.samples]))
 
 (defonce client (osc-client "localhost" 4558))
 
-(dotimes [i 20]
-  (Thread/sleep 5000)
-  (osc-send client (choose ["/flip" "/flop"]) ""))
+(dotimes [i 100]
+  (Thread/sleep 250)
+  (kick-s)
+  (osc-send client "/beat" ""))
