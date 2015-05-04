75544cfccc99729fb1a3f6c6dd6f7b34701f317f
Joseph Wilk <joe@josephwilk.net>
2015-05-04 20:10:31 +0200

    custom sample player

--- src/cassiopeia/waves/synths.clj
+++ src/cassiopeia/waves/synths.clj
@@ -27,6 +27,23 @@
     (detect-silence:ar (+ inv-trg b) 0.001 10 FREE)
     (out out-bus (* amp output))))
 
+(defsynth play-buf
+  "Plays a stereo buffer from start pos to end pos (represented as
+   values between 0 and 1). May be looped via the loop?
+   argument. Release time is the release phase after the looping has
+   finished to remove clipping."
+  [buf 0 rate 1 start 0 end 1 loop? 0 amp 1 release 0.1 out-bus 0]
+  (let [n-frames  (buf-frames buf)
+        rate      (* rate (buf-rate-scale buf))
+        start-pos (* start n-frames)
+        end-pos   (* end n-frames)
+        phase     (phasor:ar :start start-pos :end end-pos :rate rate)
+        snd       (buf-rd 2 buf phase)
+        e-gate    (+ loop?
+                     (a2k (latch:ar (line 1 0 0.0001) (bpz2 phase))))
+        env       (env-gen (adsr 8.0 1.5 1.1 release) :gate e-gate :action FREE)]
+    (out out-bus (* amp env snd))))
+
 (defsynth grainy-buf [b 0 amp 1 c-dur 10 trate 2]
   (let [dur (/ 1.2 trate)
         src (t-grains:ar :num-channels 1
