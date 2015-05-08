56cdd1b34953dde75390571b828265767b2ef058
Joseph Wilk <joe@josephwilk.net>
2015-05-08 10:15:30 +0200

    add fl for fadeout

--- src/cassiopeia/space.clj
+++ src/cassiopeia/space.clj
@@ -8,7 +8,7 @@
 ╚═╝  ╚═╝╚══════╝╚═╝     ╚══════╝    ╚══════╝╚══════╝╚══════╝ ╚═════╝   ╚═╝   ╚═╝  ╚═╝╚═╝ ╚═════╝
 "
 (:use [overtone.live] [cassiopeia.waves.synths] [mud.core] [cassiopeia.waves.soprano] [cassiopeia.view-screen] [mud.core] [cassiopeia.engine.scheduled-sampler] [cassiopeia.samples] [cassiopeia.dirt] [cassiopeia.engine.samples])
-(:require [mud.timing :as time] [overtone.studio.fx :as fx]  [shadertone.tone :as t] [cassiopeia.engine.buffers :as b]))
+(:require [mud.timing :as time] [overtone.studio.fx :as fx]  [shadertone.tone :as t] [cassiopeia.engine.buffers :as b] [cassiopeia.destination.flatiron.utils :as fl]))
 
 (ctl time/root-s :rate 4)
 
