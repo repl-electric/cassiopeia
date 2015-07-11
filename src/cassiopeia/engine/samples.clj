652646554b40fcbb6aa6dceabfe647ae60be86d7
Joseph Wilk <joe@josephwilk.net>
2015-07-11 10:22:29 +0200

    sample lookup

--- src/cassiopeia/engine/samples.clj
+++ src/cassiopeia/engine/samples.clj
@@ -5,3 +5,13 @@
 
 (defn load-local-sample     [sample] (load-sample (str SAMPLE-ROOT sample)))
 (defn local-recording-start [name]   (recording-start (str SAMPLE-ROOT name)))
+
+(defonce directory (clojure.java.io/file "/Users/josephwilk/Workspace/music/samples/"))
+(defonce files (file-seq directory))
+
+(defn find-sample [match idx]
+  (let [r (->> (filter #(and
+                         (re-find #"\.wav" (.getName %))
+                         (re-find (re-pattern (str "(?i)" match)) (.getName %))) files)
+               (map #(.getAbsolutePath %1)))]
+    (nth r (mod idx (count r)))))
