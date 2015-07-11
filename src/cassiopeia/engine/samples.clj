05534cfe51cd53d0eea04ed54f130a0ba8d72119
Joseph Wilk <joe@josephwilk.net>
2015-07-11 14:15:59 +0200

    cache + add my favourites

--- src/cassiopeia/engine/samples.clj
+++ src/cassiopeia/engine/samples.clj
@@ -1,17 +1,26 @@
 (ns cassiopeia.engine.samples
   (:use overtone.live))
 
+(defonce __sample_cache__ (atom {}))
 (def SAMPLE-ROOT "/Users/josephwilk/Dropbox/repl-electric/samples/")
 
 (defn load-local-sample     [sample] (load-sample (str SAMPLE-ROOT sample)))
 (defn local-recording-start [name]   (recording-start (str SAMPLE-ROOT name)))
 
-(defonce directory (clojure.java.io/file "/Users/josephwilk/Workspace/music/samples/"))
-(defonce files (file-seq directory))
+(defonce directory-for-samples (clojure.java.io/file "/Users/josephwilk/Workspace/music/samples/"))
+(defonce all-files-samples (file-seq directory-for-samples))
+(defonce ether-set (file-seq (clojure.java.io/file "/Users/josephwilk/Workspace/music/samples/Ether")))
+(defonce mountain-set (file-seq (clojure.java.io/file "/Users/josephwilk/Workspace/music/samples/Mountain")))
 
-(defn find-sample [match idx]
-  (let [r (->> (filter #(and
-                         (re-find #"\.wav" (.getName %))
-                         (re-find (re-pattern (str "(?i)" match)) (.getName %))) files)
-               (map #(.getAbsolutePath %1)))]
-    (nth r (mod idx (count r)))))
+(defn find-sample
+([match idx] (find-sample match idx all-files-samples))
+([match idx sample-files]
+     (let [sample-key (str match ":" idx)]
+       (or (get sample-key @__sample_cache__)
+           (let [r  (->> (filter #(and
+                                   (re-find #"\.wav" (.getName %))
+                                   (re-find (re-pattern (str "(?i)" match)) (.getName %))) sample-files)
+                         (map #(.getAbsolutePath %1)))
+                 sample (sample  (nth r (mod idx (count r))))]
+             (swap! __sample_cache__ assoc sample-key sample)
+             sample)))))
